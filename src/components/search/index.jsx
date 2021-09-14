7644ee9c10cb61afe845f2d183fe3d3db0986f62
luna <anachrosh@gmail.com>
2021-09-14 17:06:16 +0900

    wip

--- src/components/search/index.jsx
+++ src/components/search/index.jsx
@@ -7,6 +7,19 @@ import './index.scss'
 export const Search = () => {
   const [isClicked, setIsClicked] = React.useState(false)
 
+  const handleClickWindow = () => {
+    console.log('window')
+  }
+
+  React.useEffect(() => {
+    console.log(isClicked)
+    if (isClicked) {
+      window.addEventListener('click', handleClickWindow)
+    } else if (!isClicked) {
+      window.removeEventListener('click', handleClickWindow)
+    }
+  }, [isClicked])
+
   return (
     <section
       className={isClicked ? 'search-container active' : 'search-container'}
